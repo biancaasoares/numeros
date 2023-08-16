4ab9ba85dcb6b1dfa3a235c540bac25f39692a13
Bianca Soares <biannca01soares@gmail.com>
2023-08-16 17:09:52 -0300

    numeros e media

--- main.js
+++ main.js
@@ -2,6 +2,9 @@
 
 const botaoSomar = document.getElementById('somar');
 const botaoIdentificar = document.getElementById('identificar');
+const botaoMedia = document.getElementById('media');
+
+
 
 function somar (){
     const numero1 = Number (document.getElementById('numero1').value)
@@ -25,5 +28,27 @@ function identificar (){
     }
 }
 
+function media(){
+    const nota1 = Number (document.getElementById('nota1').value)
+    const nota2 = Number  (document.getElementById('nota2').value)
+    const nota3 = Number  (document.getElementById('nota3').value)
+    const nota4 = Number  (document.getElementById('nota4').value)
+    const media = document.getElementById('media')
+
+    const mediaa = (nota1 + nota2 + nota3 + nota4) / 4
+
+    media.textContent = mediaa
+    const resultado = document.getElementById('resultado-item3')
+    if (mediaa > 7){
+        resultado.textContent = 'APROVADO'
+    } else {
+        resultado.textContent = 'REPROVADO'
+    }
+
+
+} 
+
+
 botaoSomar.addEventListener('click', somar)
 botaoIdentificar.addEventListener('click', identificar)
+botaoMedia.addEventListener('click', media )       
